f1a257cb51e39a4a3eb6fa2061e9bca07048245e
zLucky090802 <d.espitia15@gmail.com>
2026-09-15 13:46:39 -0500

    feature: get_organizations service method added

--- app/services/organization_service.py
+++ app/services/organization_service.py
@@ -1,5 +1,13 @@
-from app.repositories.organization_repository import create_organization,delete_organization, get_organization_by_id, get_organizations
+from app.repositories import organization_repository
 from app.db.database import get_db
 from sqlalchemy.orm import Session
 from fastapi import Depends
 
+def get_organizations(db:Session):
+    organizations = organization_repository.get_organizations(db)
+    
+    return {
+        'success': True,
+        'message':'Organizations retrieved succesfully',
+        'data': organizations
+    }
